d8e06350cb619d63e651ea41cc37b8ffeb137c5b
persephoneslove <x.persephones.love.x@gmail.com>
2026-08-21 23:03:12 -0500

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/bae-peptides/',
   server: {
-    host: true, // Exposes on local network so you can open on iPhone via local IP!
+    host: true,
     port: 5173
   }
 })
